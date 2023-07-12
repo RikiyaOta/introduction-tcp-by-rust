65b0835a0556444c95fe82296679222f164c1131
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-07-13 00:00:24 +0900

    Add comments.

--- echoserver/src/main.rs
+++ echoserver/src/main.rs
@@ -11,20 +11,22 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn echo_server(address: &str) -> Result<(), Box<dyn Error>> {
-    let listener = TcpListener::bind(address)?;
+    let listener = TcpListener::bind(address)?; // [1]
 
     loop {
-        let (mut stream, _) = listener.accept()?;
+        let (mut stream, _) = listener.accept()?; // [2]
 
         thread::spawn(move || {
+            // [3]
             let mut buffer = [0u8; 1024];
             loop {
-                let nbytes = stream.read(&mut buffer).unwrap();
+                let nbytes = stream.read(&mut buffer).unwrap(); // [4]
                 if nbytes == 0 {
+                    // [6]
                     return;
                 }
                 print!("{}", str::from_utf8(&buffer[..nbytes]).unwrap());
-                stream.write_all(&buffer[..nbytes]).unwrap();
+                stream.write_all(&buffer[..nbytes]).unwrap(); // [5]
             }
         });
     }
