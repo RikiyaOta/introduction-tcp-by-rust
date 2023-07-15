0d2953591884f7c6b195433f134daa6e23e9e401
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-07-15 16:16:34 +0900

    ToyTCP

--- toytcp/src/tcp.rs
+++ toytcp/src/tcp.rs
@@ -18,3 +18,41 @@ const MAX_TRANSMITTION: u8 = 5;
 const RETRANSMITTION_TIMEOUT: u64 = 3;
 const MSS: usize = 1460;
 const PORT_RANGE: Range<u16> = 40000..60000;
+
+pub struct TCP {
+    sockets: HashMap<SockID, Socket>,
+}
+
+impl TCP {
+    pub fn new() -> Self {
+        Self {
+            sockets: HashMap::new(),
+        }
+    }
+
+    /// TODO: 後でハードコードしているのを変更する。
+    fn select_unused_port(&self, rng: &mut ThreadRng) -> Result<u16> {
+        Ok(33445)
+    }
+
+    pub fn connect(&self, addr: Ipv4Addr, port: u16) -> Result<SockID> {
+        let mut rng = rand::thread_rng();
+        let mut socket = Socket::new(
+            get_source_addr_to(addr)?,
+            addr,
+            self.select_unused_port(&mut rng)?,
+            port,
+        )?;
+
+        // ここで SYN を送ってる。3 way handshake の最初のセグメント。
+        socket.send_tcp_packet(tcpflags::SYN, &[])?;
+        let sock_id = socket.get_sock_id();
+
+        Ok(sock_id)
+    }
+}
+
+/// TODO: 後でハードコードしているのを変更する。
+fn get_source_addr_to(addr: Ipv4Addr) -> Result<Ipv4Addr> {
+    Ok("10.0.0.1".parse().unwrap())
+}
