8e8f157f843e90efdc4fef52b6b0dd2e9a1c676d
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-07-17 11:17:09 +0900

    undo experiment

--- toytcp/examples/echoclient.rs
+++ toytcp/examples/echoclient.rs
@@ -23,10 +23,10 @@ fn echo_client(remote_addr: Ipv4Addr, remote_port: u16) -> Result<()> {
         // connect した後に stdin で受け取った文字列を send するだけ。
         let mut input = String::new();
         io::stdin().read_line(&mut input)?;
+        tcp.send(sock_id, input.as_bytes())?;
         // 実験：入力した文字列を増大させて送ってみる（window が効いていることを確認するため）
-        // tcp.send(sock_id, input.as_bytes())?;
-        loop {
-            tcp.send(sock_id, input.repeat(2000).as_bytes())?;
-        }
+        //loop {
+        //    tcp.send(sock_id, input.repeat(2000).as_bytes())?;
+        //}
     }
 }
